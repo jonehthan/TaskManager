b28d34eaf9ca040e493870ac5441f4457caae80d
jonehthan <jonathanewu@gmail.com>
2025-05-04 14:40:58 -0700

    test

--- src/gui/widgets/styled_frame.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-from tkinter import ttk
-
-class StyledFrame(ttk.Frame):
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.pack(fill=tk.BOTH, expand=True)
